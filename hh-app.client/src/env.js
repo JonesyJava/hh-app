5c17953c30433951b2ad918976acde98d44a7204
Jonesy <jonesyjava@gmail.com>
2021-07-16 10:53:20 -0600

    Project init

---
+++ hh-app.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-3rr781ij.us.auth0.comA'
+export const audience = 'https://HauntedHighway.com'
+export const clientId = '2hRZpk6aWa2vWmw8c0b3xVTWz1stIN5J'
