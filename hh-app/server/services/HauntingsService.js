c8c3a2ed931813390a47003861bb36b994705036
Jonesy <jonesyjava@gmail.com>
2021-07-16 13:50:52 -0600

    Standard Haunting Service init

--- hh-app/server/services/HauntingsService.js
+++ hh-app/server/services/HauntingsService.js
@@ -3,16 +3,32 @@ import { BadRequest } from '../utils/Errors'
 
 class HauntingsService {
   async findHauntings(query = {}) {
-    const haunting = await dbContext.Haunting.find(query)
+    const haunting = await dbContext.Haunting.find(query).populate('creator')
     return haunting
   }
 
   async findHauntingById(id) {
-    const haunting = await dbContext.Haunting.findById(id)
+    const haunting = await dbContext.Haunting.findById(id).populate('creator')
     if (!haunting) {
       throw new BadRequest('Invalid Id')
     }
     return haunting
+  }
+
+  async createHaunting(data) {
+    return await dbContext.Haunting.create(data)
+  }
+
+  async editHaunting(update, req) {
+    return await dbContext.Haunting.findOneAndUpdate({ _id: update.id, creatorId: req.userInfo.id }, update, { new: true }).populate('creator')
+  }
+
+  async deleteHaunting(req) {
+    const haunting = await dbContext.Haunting.findOneAndDelete({ _id: req.params.id, creatorId: req.params.id })
+    if (!haunting) {
+      throw new BadRequest('Error: You are not the Creator of this Haunting Location.')
+    }
+    return 'Deleted'
   }
 }
 
